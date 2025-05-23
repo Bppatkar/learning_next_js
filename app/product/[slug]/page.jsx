7c8cd946c0cb92c097f53a5fde65c17fe3ca18aa
Bppatkar <bhanupratappatkar777@gmail.com>
2025-05-23 17:39:51 +0530

    changer server component into client component

--- app/product/[slug]/page.jsx
+++ app/product/[slug]/page.jsx
@@ -1,12 +1,45 @@
+// import React from "react";
+// import { products } from "../../data.js";
+
+// const page = ({ params: { slug } }) => {
+//   // console.log("printing the slug........", slug);
+//   const newProductData = products.filter((data) =>
+//     data.category.toLowerCase().includes(slug.toLowerCase())
+//   );
+//   // console.log(newProductData);
+//   return (
+//     <div className="container">
+//       <h2 className="page-title">welcome to {slug}</h2>
+//       {newProductData.map((product) => {
+//         return (
+//           <div key={product.title} className="product">
+//             <h3 className="product-title">{product.title}</h3>
+//             <p className="product-price">Price: ${product.price}</p>
+//             <p className="product-rating">
+//               User Rating: {product.userRating}⭐
+//             </p>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default page;
+//! now it becomes pure client component initially it was server component bydefault
+
+"use client";
+
 import React from "react";
 import { products } from "../../data.js";
 
-const page = ({ params: { slug } }) => {
-  // console.log("printing the slug........", slug);
+// const page = ({ params: { slug } }) => {
+const page = ({ params }) => {
+  const { slug } = React.use(params);
   const newProductData = products.filter((data) =>
     data.category.toLowerCase().includes(slug.toLowerCase())
   );
-  // console.log(newProductData);
+
   return (
     <div className="container">
       <h2 className="page-title">welcome to {slug}</h2>
@@ -15,7 +48,9 @@ const page = ({ params: { slug } }) => {
           <div key={product.title} className="product">
             <h3 className="product-title">{product.title}</h3>
             <p className="product-price">Price: ${product.price}</p>
-            <p className="product-rating">User Rating: {product.userRating}⭐</p>
+            <p className="product-rating">
+              User Rating: {product.userRating}⭐
+            </p>
           </div>
         );
       })}
